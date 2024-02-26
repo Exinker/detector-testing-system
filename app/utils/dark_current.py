b9c6e2d2de9734e139991d5f81d9ee94df6cc551
Exinker <exinker@gmail.com>
2024-02-26 18:59:41 +0700

    refac:

---
+++ app/utils/dark_current.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from libspectrum2_wrapper.alias import Array
+from libspectrum2_wrapper.units import get_units_clipping, get_units_label
+
+from core.data import Data, to_array
+
+from .utils import calculate_stats
+
+
+def calculate_current(data: Data, n: int, threshold: float = np.inf, show: bool = False) -> float:
+    """Calculate a current of the cell."""
+    u, du, tau = to_array(data=data, n=n)
+
+    # mask
+    mask = u < threshold
+
+    #
+    p = np.polyfit(tau[mask], u[mask], deg=1)
+    current = 1e+3*p[0]  # in %/s
+
+    u_hat = np.polyval(p, tau)
+
+    # show
+    if show:
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4), tight_layout=True)
+
+        plt.scatter(
+            tau, u,
+            c='grey', s=10,
+        )
+        plt.scatter(
+            tau[mask], u[mask],
+            c='red', s=10,
+        )
+        plt.plot(
+            tau, u_hat,
+            color='black', linestyle='-', linewidth=1,
+        )
+        content = [
+            r'$n$: {n:.0f}'.format(n=n),
+            r'$i$: {value:.4f} {units}'.format(
+                value=current,
+                units=f'[{get_units_label(data.units, is_enclosed=False)}/s]',
+            ),
+        ]
+        plt.text(
+            0.05/2, 0.95,
+            '\n'.join(content),
+            transform=ax.transAxes,
+            ha='left', va='top',
+        )
+        plt.xlabel(r'$\tau$ {units}'.format(units=r'[$ms$]'))
+        plt.ylabel(r'$U$ {units}'.format(units=f'[{get_units_label(data.units, is_enclosed=False)}/s]'))
+        plt.grid(color='grey', linestyle=':')
+
+        plt.show()
+
+    #
+    return current
+
+
+def research_current(data: Data, threshold: float | None = None, confidence: float = .95, show: bool = False) -> Array[float]:
+    """Calculate a current of the cells."""
+    _, n_numbers = data.mean.shape
+
+    clipping_value = get_units_clipping(units=data.units)
+    threshold = threshold or clipping_value
+
+    current = np.zeros(n_numbers,)
+    for n in range(n_numbers):
+        current[n] = calculate_current(
+            data=data, n=n,
+            threshold=threshold,
+        )
+
+    # show
+    if show:
+        mean, ci = calculate_stats(current, confidence=confidence)
+
+        #
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4), tight_layout=True)
+
+        plt.scatter(
+            range(n_numbers), current,
+            c='black', s=2,
+        )
+        content = [
+            r'$i_{{d}}$: {mean:.4f} $\pm$ {ci:.4f}'.format(
+                mean=mean,
+                ci=ci,
+            ),
+        ]
+        plt.text(
+            0.05/2, 0.95,
+            '\n'.join(content),
+            transform=ax.transAxes,
+            ha='left', va='top',
+        )
+        plt.xlabel(r'$number$')
+        plt.ylabel(r'$i_{{d}}$ {units}'.format(
+            units=f'[{get_units_label(data.units, is_enclosed=False)}/s]',
+        ))
+        plt.grid(color='grey', linestyle=':')
+
+        plt.show()
+
+    #
+    return current
--- app/utils/read_noise.py
+++ app/utils/read_noise.py
@@ -1,113 +1,201 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from distfit import distfit
 
 from libspectrum2_wrapper.alias import Array
+from libspectrum2_wrapper.units import get_units_clipping, to_electron
 
-from core.data import Data
+from core.data import Data, to_array
+
+from .bias import calculate_bias
+from .utils import calculate_stats, normalize_values, treat_outliers
 
 
-def calculate_read_noise(data: Data, n: int, show: bool = False) -> Array[float]:
-    """Calculate a read noise of the cell."""
+def calculate_read_noise(data: Data, n: int, threshold: float | None = None, verbose: bool = False, show: bool = False) -> float:
+    """Calculate noise read."""
+    clipping_value = get_units_clipping(units=data.units)
+    threshold = clipping_value if threshold is None else threshold
+    u, du, tau = to_array(data=data, n=n)
 
-    read_noise = np.concatenate([datum.intensity[:,n] for datum in data])
+    # mask
+    mask = u < threshold
+
+    #
+    p = np.polyfit(u[mask], du[mask], deg=1)
+    du_hat = np.polyval(p, u)
+
+    u_bias = calculate_bias(
+        data=data, n=n,
+        threshold=threshold,
+    )
+
+    angle = p[0]
+    capacity = np.nan if angle < 0 else clipping_value/angle
+
+    read_noise = np.sqrt(np.polyval(p, u_bias))
 
     # show
     if show:
-        fig, (ax_left, ax_right) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4), tight_layout=True)
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4))
 
-        plt.sca(ax_left)
+        plt.scatter(
+            u, du,
+            c='grey', s=10,
+        )
+        plt.scatter(
+            u[mask], du[mask],
+            c='red', s=10,
+        )
         plt.plot(
-            read_noise,
-            linestyle='none', marker='.', markersize=1,
-            color='black',
+            u, du_hat,
+            linestyle='solid', c='grey',
         )
-        plt.xlabel('time')
-        plt.ylabel(fr'$U$ {data.units_label}')
-        plt.grid(color='grey', linestyle=':')
-
-        plt.sca(ax_right)
-        plt.hist(
-            read_noise,
-            bins=150,
-            facecolor='white', edgecolor='black',
+        content = [
+            r'$U_{{b}}$: {value:.4f} {units}'.format(
+                value=u_bias,
+                units=data.units_label,
+            ),
+            r'$\sigma_{{rd}}$: {value:.4f} {units}'.format(
+                value=to_electron(
+                    read_noise,
+                    units=data.units,
+                    capacity=capacity,
+                ),
+                units=r'$[e^{-}]$',
+            ),
+        ]
+        plt.text(
+            0.05/2, 0.95,
+            '\n'.join(content),
+            transform=ax.transAxes,
+            ha='left', va='top',
         )
-        plt.xlabel(fr'$U$ {data.units_label}')
-        plt.ylabel('freq')
+        plt.xlabel(r'$U$ {units}'.format(units=data.units_label))
+        plt.ylabel(r'$\sigma^{2}$')
         plt.grid(color='grey', linestyle=':')
 
         plt.show()
+
 
     #
     return read_noise
 
 
-
-def research_read_noise(data: Data, show: bool = False) -> Array[float]:
-    """Calculate a read noise of the cells."""
+def research_read_noise(data: Data, threshold: float | None = None, show: bool = False) -> Array[float]:
+    """Research read_noise."""
     _, n_numbers = data.mean.shape
 
-    read_noise = np.mean(np.sqrt(data.variance), axis=0)
+    clipping_value = get_units_clipping(units=data.units)
+    threshold = threshold or clipping_value
+
+    read_noise = np.zeros(n_numbers,)
+    for n in range(n_numbers):
+        read_noise[n] = calculate_read_noise(
+            data=data,
+            n=n,
+            threshold=threshold,
+        )
 
     # show
     if show:
-        fig, (ax_left, ax_right) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4), tight_layout=True)
+        mean, ci = calculate_stats(read_noise)
+
+        #
+        fig, (ax_left, ax_right) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4))
 
         plt.sca(ax_left)
-        plt.plot(
-            read_noise,
-            linestyle='none', marker='.', markersize=2,
-            color='black',
+        plt.scatter(
+            range(n_numbers), read_noise,
+            c='black', s=2,
         )
-        plt.xlabel('number')
-        plt.ylabel(fr'$\sigma$ {data.units_label}')
+        plt.axhline(
+            mean,
+            color='red', linestyle='-', linewidth=2,
+        )
+        plt.xlabel(r'$number$')
+        plt.ylabel(r'read-noise {units}'.format(units=data.units_label))
         plt.grid(color='grey', linestyle=':')
+        # plt.legend()
 
         plt.sca(ax_right)
+        content = [
+            fr'$\hat{{c}}: {mean:.4f} \pm {ci:.4f}$',
+        ]
+        plt.text(
+            0.05/2, 0.95,
+            '\n'.join(content),
+            transform=ax_right.transAxes,
+            ha='left', va='top',
+        )
         plt.hist(
             read_noise,
-            bins=100,
-            facecolor='white', edgecolor='black',
+            bins=40,
+            edgecolor='black', facecolor='white',
+            # fill=False,
         )
-        plt.xlabel(fr'$\sigma$ {data.units_label}')
-        plt.ylabel('count')
+
+        plt.xlabel(r'read-noise {units}'.format(units=data.units_label))
+        plt.ylabel(r'freq')
         plt.grid(color='grey', linestyle=':')
+        # plt.legend()
 
         plt.show()
+
+    # show distribution
+    if show:
+        values = read_noise.copy()
+        values = treat_outliers(values)
+        values = normalize_values(values)
+
+        # show
+        dfit = distfit(
+            distr='norm',
+        )
+
+        result = dfit.fit_transform(
+            values,
+            verbose=False,
+        )
+
+        #
+        fig, (ax_left, ax_right) = plt.subplots(nrows=1, ncols=2, figsize=(18, 6))
+
+        dfit.plot(
+            chart='pdf',
+            ax=ax_left,
+        )
+        dfit.qqplot(
+            values,
+            ax=ax_right,
+        )
 
     #
     return read_noise
 
 
-def research_relative_read_noise(data: Data, show: bool = False) -> Array[float]:
-    """Calculate a read noise of the cells."""
+def compare_noise(read_noise: Array[float], data: Data) -> None:
+    """"""
+    _, n_numbers = data.mean.shape
 
-    read_noise = 100 * np.std(np.sqrt(data.variance), ddof=1, axis=0) / np.mean(np.sqrt(data.variance), axis=0)
-
-    # show
-    if show:
-        fig, (ax_left, ax_right) = plt.subplots(nrows=1, ncols=2, figsize=(12, 4), tight_layout=True)
-
-        plt.sca(ax_left)
-        plt.plot(
-            read_noise,
-            linestyle='none', marker='.', markersize=2,
-            color='black',
-        )
-        plt.xlabel('number')
-        plt.ylabel(fr'$\sigma / \Delta\sigma$ {data.units_label}')
-        plt.grid(color='grey', linestyle=':')
-
-        plt.sca(ax_right)
-        plt.hist(
-            read_noise,
-            bins=100,
-            facecolor='white', edgecolor='black',
-        )
-        plt.xlabel(fr'$\sigma / \Delta\sigma$ {data.units_label}')
-        plt.ylabel('freq')
-        plt.grid(color='grey', linestyle=':')
-
-        plt.show()
+    noise = np.mean(np.sqrt(data.variance), axis=0)
 
     #
-    return read_noise
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4))
+
+    # plt.scatter(
+    #     range(n_numbers), read_noise,
+    #     c='black', s=2,
+    # )
+    plt.axhline(
+        np.mean(read_noise),
+        color='black', linestyle=':',
+    )
+    plt.scatter(
+        range(n_numbers), noise,
+        c='red', s=2,
+    )
+    plt.xlabel(r'$number$')
+    plt.ylabel(r'noise-read {units}'.format(units=data.units_label))
+    plt.grid(color='grey', linestyle=':')
+
+    plt.show()
